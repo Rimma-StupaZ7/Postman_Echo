88a50e291fd05b15cf9eb9d545021db0ca6a287e
rimma test <valentin.rimma@gmail.com>
2023-05-08 18:12:43 +0400

    remove comments

--- src/test/java/ru/netology/rest/PostManTestPost.java
+++ src/test/java/ru/netology/rest/PostManTestPost.java
@@ -4,16 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+
 public class PostManTestPost {
     @Test
-    void QueryPostData(){
+    void QueryPostData() {
         given()
                 .baseUri("https://postman-echo.com")
-                .body("John") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                .body("John")
                 .when()
                 .post("/post")
-// Проверки
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("John"));
